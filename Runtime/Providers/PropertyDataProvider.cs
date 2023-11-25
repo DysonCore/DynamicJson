05e390929f49901dcdb6c87a2575bcfdac4913ee
DysonCore <bogdan.mezentsev@gmail.com>
2023-11-25 20:46:06 +0100

    summaries added to PropertyDataProvider

--- Runtime/Providers/PropertyDataProvider.cs
+++ Runtime/Providers/PropertyDataProvider.cs
@@ -9,14 +9,37 @@ using Newtonsoft.Json;
 
 namespace DysonCore.PolymorphicJson.Providers
 {
+    /// <summary>
+    /// Provides a centralized data provider for managing <see cref="PropertyData"/> mappings used by polymorphic converter.
+    /// It handles mappings of abstract types and their concrete implementations to facilitate correct type resolution during deserialization.
+    /// </summary>
     internal static class PropertyDataProvider
     {
+        /// <summary>
+        /// Stores mappings from base types to their corresponding <see cref="TypifyingPropertyData"/>.
+        /// Used to resolve the correct type during JSON deserialization based on <see cref="TypifyingPropertyAttribute"/>.
+        /// </summary>
         internal static Dictionary<Type, TypifyingPropertyData> BaseToPropertyData { get; } = new ();
-        
+        /// <summary>
+        /// Temporal list of tuples containing abstract types of secondary inheritance (when abstract class assigns value to the abstract property of base class) and associated <see cref="TypifyingPropertyData"/>.
+        /// Used to store additional data required for <see cref="PostProcessAbstractClasses"/>. Gets cleared when <see cref="InitializeCache"/> is finished.
+        /// </summary>
         private static readonly List<(Type abstractType, TypifyingPropertyData propertyData)> AbstractDefiningData = new ();
+        /// <summary>
+        /// Temporal dictionary of types to a list of <see cref="TypifiedPropertyData"/>, storing information for types that have been marked with <see cref="TypifiedPropertyAttribute"/>.
+        /// Used to store additional data required for <see cref="PostProcessTypifiedProperties"/>. Gets cleared when <see cref="InitializeCache"/>> is finished.
+        /// </summary>
         private static readonly Dictionary<Type, List<TypifiedPropertyData>> TypifiedDefiningData = new ();
+        /// <summary>
+        /// Indicates whether the PropertyDataProvider has been initialized, to prevent redundant initializations.
+        /// </summary>
         private static bool _initialized;
         
+        /// <summary>
+        /// Initializes the <see cref="PropertyDataProvider"/> with data from the specified assemblies.
+        /// Scans the provided assemblies to build the data mappings required for polymorphic deserialization.
+        /// </summary>
+        /// <param name="assembliesToUse">The assemblies to scan for <see cref="TypifyingPropertyAttribute"/> and <see cref="TypifiedPropertyAttribute"/>. If left empty - all the assemblies which are referencing this assembly will be scanned.</param>
         internal static void Initialize(params Assembly[] assembliesToUse)
         {
             if (assembliesToUse.Length > 0)
@@ -36,8 +59,8 @@ namespace DysonCore.PolymorphicJson.Providers
         }
         
         /// <summary>
-        /// Initializes provider by scanning the given assemblies for types with the <see cref="TypifyingPropertyAttribute"/> 
-        /// and populates the BaseToPropertyData dictionary.
+        /// Initializes provider by scanning the given assemblies for types with the <see cref="TypifyingPropertyAttribute"/>
+        /// and populates the <see cref="BaseToPropertyData"/> dictionary.
         /// </summary>
         /// <param name="assemblies">List of assemblies to scan.</param>
         private static void InitializeCache(params Assembly[] assemblies)
@@ -62,6 +85,12 @@ namespace DysonCore.PolymorphicJson.Providers
             TypifiedDefiningData.Clear();
         }
 
+        /// <summary>
+        /// Processes a property to determine whether it has <see cref="TypifyingPropertyAttribute"/> or <see cref="TypifiedPropertyAttribute"/> and ensures that it is not marked as both.
+        /// </summary>
+        /// <param name="propertyInfo">The PropertyInfo object representing the property to process.</param>
+        /// <param name="classType">The Type of the class that the property belongs to.</param>
+        /// <exception cref="Exception">Throws the <see cref="Exception"/> if single property marked with both <see cref="TypifyingPropertyAttribute"/> and <see cref="TypifiedPropertyAttribute"/></exception>
         private static void ProcessProperty(PropertyInfo propertyInfo, Type classType)
         {
             TypifyingPropertyAttribute typifyingAttribute = propertyInfo.GetCustomAttribute<TypifyingPropertyAttribute>();
@@ -82,11 +111,18 @@ namespace DysonCore.PolymorphicJson.Providers
 
             if (isTypified)
             {
-                ProcessTypifiedAttribute(propertyInfo, classType, typifiedAttribute);
+                ProcessTypifiedProperty(propertyInfo, classType, typifiedAttribute);
             }
         }
-
-
+        
+        /// <summary>
+        /// Processes properties marked with <see cref="TypifyingPropertyAttribute"/> to establish their role in polymorphic deserialization.
+        /// This method also ensures the consistency of typifying properties in base and derived classes by validating
+        /// the attribute's presence in the base class, thereby supporting correct type resolution in polymorphic scenarios.
+        /// </summary>
+        /// <param name="propertyInfo">The <see cref="PropertyInfo"/> object of the property with <see cref="TypifyingPropertyAttribute"/></param>
+        /// <param name="classType">The Type of the class that contains the current property with <see cref="TypifyingPropertyAttribute"/>.</param>
+        /// <param name="typifyingAttribute">The <see cref="TypifyingPropertyAttribute"/> applied to the property.</param>
         private static void ProcessTypifyingProperty(PropertyInfo propertyInfo, Type classType, TypifyingPropertyAttribute typifyingAttribute)
         {
             Type propertyType = propertyInfo.PropertyType;
@@ -122,7 +158,15 @@ namespace DysonCore.PolymorphicJson.Providers
             propertyData.ValuesData[propertyValue] = classType;
         }
 
-        private static void ProcessTypifiedAttribute(PropertyInfo propertyInfo, Type classType, TypifiedPropertyAttribute typifiedAttribute)
+        /// <summary>
+        /// Processes properties marked with <see cref="TypifiedPropertyAttribute"/>, capturing their serialization details.
+        /// The method adds information about <see cref="TypifiedPropertyAttribute"/> to the <see cref="TypifiedDefiningData"/>, which is essential
+        /// for handling custom serialization and deserialization behaviors of these composed properties based on <see cref="TypifyingPropertyAttribute"/> of the holder class.
+        /// </summary>
+        /// <param name="propertyInfo">The <see cref="PropertyInfo"/> object of the property with <see cref="TypifiedPropertyAttribute"/>.</param>
+        /// <param name="classType">The Type of the class that contains the the property with <see cref="TypifiedPropertyAttribute"/>.</param>
+        /// <param name="typifiedAttribute">The <see cref="TypifiedPropertyAttribute"/> applied to the property.</param>
+        private static void ProcessTypifiedProperty(PropertyInfo propertyInfo, Type classType, TypifiedPropertyAttribute typifiedAttribute)
         {
             Type propertyType = propertyInfo.PropertyType;
             JsonPropertyAttribute jsonProperty = propertyInfo.GetCustomAttribute<JsonPropertyAttribute>();
@@ -137,6 +181,11 @@ namespace DysonCore.PolymorphicJson.Providers
             propertyData.Add(data);
         }
 
+        /// <summary>
+        /// Iterates through <see cref="AbstractDefiningData"/> and establishes the mapping between abstract classes and their <see cref="TypifyingPropertyAttribute"/> value.
+        /// This method is crucial for enabling polymorphic deserialization where abstract types of secondary inheritance (abstract class assigns value to the base abstract property) need to be resolved
+        /// to their concrete implementations.
+        /// </summary>
         private static void PostProcessAbstractClasses()
         {
             foreach (var data in AbstractDefiningData)
@@ -161,6 +210,10 @@ namespace DysonCore.PolymorphicJson.Providers
             }
         }
 
+        /// <summary>
+        /// Finalizes the processing of <see cref="TypifiedPropertyAttribute"/> by incorporating them into the <see cref="BaseToPropertyData"/>.
+        /// This method finds corresponding <see cref="TypifyingPropertyData"/> for all <see cref="TypifiedPropertyAttribute"/>s and adds them in. 
+        /// </summary>
         private static void PostProcessTypifiedProperties()
         {
             foreach (var data in TypifiedDefiningData)
